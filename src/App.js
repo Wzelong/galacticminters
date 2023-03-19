642524f1f3ecf53218390602f7ea1f11493fccaf
Zelong Wang <wang2447@wisc.edu>
2023-03-19 18:50:23 -0500

    setup app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Metamask from './Metamask';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Metamask/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
