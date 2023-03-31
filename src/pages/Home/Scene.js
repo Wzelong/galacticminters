1532f0f3b29685fb2690a8803804fae6053f221d
Zelong Wang <wang2447@wisc.edu>
2023-03-31 13:52:31 -0500

    Update Scene.js

--- src/pages/Home/Scene.js
+++ src/pages/Home/Scene.js
@@ -10,10 +10,9 @@ function Scene() {
 
   return (
     <>
-      <Spline scene="https://prod.spline.design/qOEgWG80rQnifgb4/scene.splinecode" />
       <Canvas
         camera={{ position: [0, 15, 90] }}
-        style={{ backgroundColor: "rgb(0, 0, 0)" }}
+        style={{ backgroundColor: "rgb(0, 0, 0)", position: "absolute", top: 0, left: 0, zIndex: -1  }}
       >
         <Stars
           radius={200}
@@ -27,6 +26,7 @@ function Scene() {
         <ambientLight intensity={0.1} />
         <OrbitControls makeDefault maxDistance={100} />
       </Canvas>
+      <Spline scene="https://prod.spline.design/qOEgWG80rQnifgb4/scene.splinecode" style={{position: "absolute", widhth: "100%", height: "100vh", top: 0, left: 0, zIndex: 0, backgroundColor:"rgb(0,0,0,0)"}} />
     </>
   );
 }
